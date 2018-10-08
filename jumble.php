d7a2301f8f0cd9c5da7abe8fabae59526b1b8790
Jeremy Keeshin <jkeesh@gmail.com>
2018-10-08 01:29:39 -0400

    update https

--- jumble.php
+++ jumble.php
@@ -18,7 +18,7 @@ $cleaned = strtoupper($cleaned);
             Jumble Maker!
         </title>
 
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
     <script type="text/javascript" src="LetterContainer.js"></script>
     <script type="text/javascript" src="WordContainer.js"></script>
